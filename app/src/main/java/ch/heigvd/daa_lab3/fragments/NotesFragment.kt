fe42143774e92f8b56de6c49773a2076e2827478
Stéphane Marengo <stephane.marengo@hotmail.com>
2022-12-07 19:53:25 +0100

    Utilisation des SharedPreferences pour l'état du tri

--- app/src/main/java/ch/heigvd/daa_lab3/fragments/NotesFragment.kt
+++ app/src/main/java/ch/heigvd/daa_lab3/fragments/NotesFragment.kt
@@ -1,9 +1,11 @@
 package ch.heigvd.daa_lab3.fragments
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.edit
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -37,18 +39,11 @@ class NotesFragment : Fragment() {
         return inflater.inflate(R.layout.notes_fragment, container, false)
     }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putSerializable(SORTED_BY_KEY, notesAdapter.sortedBy)
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        savedInstanceState?.let {
-            notesAdapter.sortedBy =
-                it.getSerializable(SORTED_BY_KEY, NotesAdapter.SortType::class.java)!!
-        }
+        restoreSortType()
+
         view.findViewById<RecyclerView>(R.id.notes_list).apply {
             adapter = notesAdapter
             layoutManager = LinearLayoutManager(context)
@@ -63,13 +58,40 @@ class NotesFragment : Fragment() {
      * Trie la liste des notes selon la date de leur schedule.
      */
     fun sortByEta() {
-        notesAdapter.sortedBy = NotesAdapter.SortType.ETA
+        NotesAdapter.SortType.ETA.let {
+            notesAdapter.sortedBy = it
+            saveSortType(it)
+        }
     }
 
     /**
      * Trie la liste des notes selon leur date de création.
      */
     fun sortByCreationDate() {
-        notesAdapter.sortedBy = NotesAdapter.SortType.CREATION_DATE
+        NotesAdapter.SortType.CREATION_DATE.let {
+            notesAdapter.sortedBy = it
+            saveSortType(it)
+        }
+    }
+
+    /**
+     * Restaure le type de tri si ce dernier existe dans les préférences.
+     */
+    private fun restoreSortType() {
+        requireActivity().getPreferences(Context.MODE_PRIVATE)
+            .getString(SORTED_BY_KEY, null)
+            ?.let {
+                notesAdapter.sortedBy = NotesAdapter.SortType.valueOf(it)
+            }
+    }
+
+    /**
+     * Sauvegarde le type de tri donné dans les préférences.
+     * @param sortType le type de tri à sauvegarder.
+     */
+    private fun saveSortType(sortType: NotesAdapter.SortType) {
+        requireActivity().getPreferences(Context.MODE_PRIVATE).edit {
+            putString(SORTED_BY_KEY, sortType.name)
+        }
     }
 }
